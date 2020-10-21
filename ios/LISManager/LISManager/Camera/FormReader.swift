8f46a37dee2522fc1800fa51de34c6cfc80e61ed
Nick Robison <nick@nickrobison.com>
2020-10-21 17:03:41 -0400

    Added simple handler for detecting text in our example USDS form.
    The results are garbage, but it does work.

---
+++ ios/LISManager/LISManager/Camera/FormReader.swift
@@ -0,0 +1,57 @@
+//
+//  FormReader.swift
+//  LISManager
+//
+//  Created by Nicholas Robison on 10/21/20.
+//
+
+import os
+import Foundation
+import Vision
+import UIKit
+
+private let logger = OSLog(subsystem: "com.nickrobison.iot_list.LISManager.FormReader", category: "input")
+
+class FormReader: NSObject {
+    
+    
+    
+    func extractText(_ image: UIImage, handler: @escaping (String) -> Void) {
+        let request = VNRecognizeTextRequest { (r, error) in
+            guard error == nil else {
+                os_log("Error detecting text: %s", log: logger, type: .error, error!.localizedDescription)
+                return
+            }
+            
+            guard let observations = r.results as? [VNRecognizedTextObservation] else {
+                os_log("No observations in image", log: logger, type: .debug)
+                return
+            }
+            
+            os_log("I have %d observations", log: logger, type: .debug, observations.count)
+            
+            var stringResults: [String] = []
+            
+            for observation in observations {
+                guard let best = observation.topCandidates(1).first else {
+                    os_log("No recognized candidates", log: logger, type: .debug)
+                    return
+                }
+                os_log("Candidate: %s", log: logger, type: .debug, best.string)
+                stringResults.append(best.string)
+                
+            }
+            handler(stringResults.joined(separator: "\n"))
+        }
+        request.recognitionLevel = .accurate
+        
+        DispatchQueue.global(qos: .userInitiated).async {
+            let handler = VNImageRequestHandler(cgImage: image.cgImage!, options: [:])
+            try? handler.perform([request])
+        }
+    }
+    
+    private func handleDetection(request: VNRequest, error: Error?) {
+        
+    }
+}
--- ios/LISManager/LISManagerTests/LISManagerTests.swift
+++ ios/LISManager/LISManagerTests/LISManagerTests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+import UIKit
 @testable import LISManager
 
 class LISManagerTests: XCTestCase {
@@ -22,11 +23,22 @@ class LISManagerTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
+    
+    func testImageDetection() throws {
+        guard let img = UIImage(named: "SampleUSDSForm") else {
+            fatalError("Cannot find image")
+        }
+        let reader = FormReader()
+        
         self.measure {
-            // Put the code you want to measure the time of here.
+            let expectation = self.expectation(description: #function)
+            var result: String?
+            reader.extractText(img) { value in
+                result = value
+                expectation.fulfill()
+            }
+            waitForExpectations(timeout: 10)
+            XCTAssertEqual(result?.count, 364)
         }
     }
 
