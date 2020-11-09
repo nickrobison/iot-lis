c4e4cadfbcfa4c9ca8bffc4ccab9ca9b3308abab
Nick Robison <nick@nickrobison.com>
2020-11-09 10:33:49 -0500

    Wired SampleDetector into the camera view.
    It's not really working, but some things are being displayed on the screen.

---
+++ ios/LISManager/LISManager/Camera/SampleScanner.swift
@@ -0,0 +1,135 @@
+//
+//  SampleScanner.swift
+//  LISManager
+//
+//  Created by Nick Robison on 11/9/20.
+//
+
+import Foundation
+import UIKit
+import AVFoundation
+import Combine
+
+class SampleScanner: NSObject, AVCaptureVideoDataOutputSampleBufferDelegate, ObservableObject {
+    
+    @Published var inference: Inference?
+    var captureSession: AVCaptureSession?
+    var camera: AVCaptureDevice?
+    var cameraInput: AVCaptureDeviceInput?
+    var previewLayer: AVCaptureVideoPreviewLayer?
+    private lazy var videoDataOutput = AVCaptureVideoDataOutput()
+    var completionHandler: (([Inference]) -> Void)?
+    var cancellables: [AnyCancellable] = []
+    var maskLayer: CAShapeLayer?
+    
+    private let detector: SampleDetector
+    
+    init(_ completionHandler: (([Inference]) -> Void)?) {
+        self.completionHandler = completionHandler
+        self.detector = TFSampleDetector(modelInfo: MobileNetSSD.modelInfo, labelInfo: MobileNetSSD.labelsInfo)!
+    }
+    
+    func prepare(completionHandler: @escaping (Error?) -> Void) {
+        func createCaptureSession() {
+            self.captureSession = AVCaptureSession()
+        }
+        
+        func configureCaptureDevices() throws {
+            self.camera = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back)
+            
+            try self.camera?.lockForConfiguration()
+            self.camera?.unlockForConfiguration()
+            
+        }
+        func configureDeviceInputs() throws {
+            guard let captureSession = self.captureSession else { throw CameraControllerError.captureSessionIsMissing }
+            
+            if let frontCamera = self.camera {
+                self.cameraInput = try AVCaptureDeviceInput(device: frontCamera)
+                
+                if captureSession.canAddInput(self.cameraInput!) {
+                    captureSession.addInput(self.cameraInput!)
+                } else { throw CameraControllerError.inputsAreInvalid }
+                
+                // Enable video input
+                
+                let sampleBufferQueue = DispatchQueue(label: "sampleBufferQueue")
+                self.videoDataOutput.setSampleBufferDelegate(self, queue: sampleBufferQueue)
+                videoDataOutput.alwaysDiscardsLateVideoFrames = true
+                videoDataOutput.videoSettings = [ String(kCVPixelBufferPixelFormatTypeKey): kCMPixelFormat_32BGRA]
+                
+                if captureSession.canAddOutput(videoDataOutput) {
+                    self.captureSession?.addOutput(videoDataOutput)
+                    videoDataOutput.connection(with: .video)?.videoOrientation = .portrait
+                } else {
+                    throw CameraControllerError.invalidOperation
+                }
+                
+            } else { throw CameraControllerError.noCamerasAvailable }
+            
+            captureSession.startRunning()
+            
+        }
+        
+        DispatchQueue(label: "prepare").async {
+            do {
+                createCaptureSession()
+                try configureCaptureDevices()
+                try configureDeviceInputs()
+            } catch {
+                DispatchQueue.main.async {
+                    completionHandler(error)
+                }
+                return
+            }
+            
+            DispatchQueue.main.async {
+                completionHandler(nil)
+            }
+        }
+    }
+    
+    func displayPreview(on view: UIView) throws {
+        guard let captureSession = self.captureSession, captureSession.isRunning else {
+            throw CameraControllerError.captureSessionIsMissing
+        }
+        
+        self.previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        self.previewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        self.previewLayer?.connection?.videoOrientation = .portrait
+        
+        view.layer.insertSublayer(self.previewLayer!, at: 0)
+        self.previewLayer?.frame = view.frame
+    }
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        guard let pixelBuffer: CVPixelBuffer =  CMSampleBufferGetImageBuffer(sampleBuffer) else {
+            return
+        }
+        
+        self.detector.runModel(onFrame: pixelBuffer).receive(on: RunLoop.main).sink(receiveValue: { value in
+            switch value {
+            case .success(let inferences):
+                self.drawInferenceBoxes(inferences)
+            case .failure(let error):
+                debugPrint(error)
+            }
+        }).store(in: &self.cancellables)
+        
+    }
+    
+    private func drawInferenceBoxes(_ inferences: [Inference]) {
+        guard let inference = inferences.first else {
+            return
+        }
+        debugPrint("I have values from detector")
+        self.maskLayer = CAShapeLayer()
+        self.maskLayer!.frame = inference.rect
+        self.maskLayer!.cornerRadius = 10
+        self.maskLayer!.opacity = 0.75
+        self.maskLayer!.borderColor = inference.displayColor.cgColor
+        self.maskLayer!.borderWidth = 5.0
+        
+        self.previewLayer!.insertSublayer(self.maskLayer!, at: 1)
+    }
+}
